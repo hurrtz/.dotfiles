24f450b19c0930eb5d0f53794dfbb37baf58fb8d
Tobias Winkler <tobias.winkler@hqplus.de>
2017-11-30 09:19:06 +0100

    initial setup
    Signed-off-by: Tobias Winkler <tobias.winkler@hqplus.de>

---
+++ .bashrc
@@ -0,0 +1,6 @@
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+
+echo "hallo"
---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+# needed because default would be some ugly cyan background with white color
+set -g fish_pager_color_progress 'cyan'
+
+# alias for git
+alias gi=git
+
+# autoload nvm and current node version
+set -x NVM_DIR ~/.nvm
+source ~/.config/fish/nvm-wrapper/nvm.fish
+nvm use node
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
