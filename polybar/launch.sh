1b38e573b7c13fa666acd5ae867d2e28e0ef45f7
Tobias Winkler <info@tobiaswinkler.berlin>
2018-10-14 23:08:26 +0200

    adopted i3 for multi monitor
    Signed-off-by: Tobias Winkler <info@tobiaswinkler.berlin>

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar float &
-#polybar bar2 &
+polybar primary &
+polybar secondary &
 
 echo "Bars launched..."
