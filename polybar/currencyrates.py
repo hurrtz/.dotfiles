f9964dbe39c3c0352609289f91155fd30f6e6923
Tobias Winkler <tobias.winkler@omio.com>
2021-01-11 15:19:58 +0100

    cleanup i3, proper rofi exit menu, customized dunst notifications
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

--- polybar/currencyrates.py
+++ polybar/currencyrates.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import json,sys,urllib.request
--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+# wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-echo "Bars launched..."
+polybar main 2>&1 | tee -a /tmp/polybar_main.log & disown
+polybar secondary 2>&1 | tee -a /tmp/polybar_secondary.log & disown
+
+echo "bars launched..."
